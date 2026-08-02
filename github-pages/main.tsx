b88b2aeef833cdca921e38eedbd1bd9ee645fcac
Javen <118031308+zjw27@users.noreply.github.com>
2026-08-03 07:37:59 +0800

    Fix local and GitHub Pages builds (#1)

--- github-pages/main.tsx
+++ github-pages/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "../app/globals.css";
-import Home from "../src/pages/Home";
+import Home from "../src/views/Home";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
